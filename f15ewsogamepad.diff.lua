13482d384a10c04f4f3bd55eaee5cc08af10f336
Niklas Furberg <niklas.furberg@gmail.com>
2025-03-08 16:47:18 +0100

    f15 wso gamepad input layout, f15 warmup missions

---
+++ f15ewsogamepad.diff.lua
@@ -0,0 +1,501 @@
+local diff = {
+	["axisDiffs"] = {
+		["a2001cdnil"] = {
+			["name"] = "Pitch",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_Y",
+				},
+			},
+		},
+		["a2002cdnil"] = {
+			["name"] = "Roll",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_X",
+				},
+			},
+		},
+		["a2003cdnil"] = {
+			["name"] = "Rudder",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_RZ",
+				},
+			},
+		},
+		["a2004cdnil"] = {
+			["name"] = "Thrust",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_Z",
+				},
+			},
+		},
+		["a3225cd4"] = {
+			["added"] = {
+				[1] = {
+					["filter"] = {
+						["curvature"] = {
+							[1] = 0,
+						},
+						["deadzone"] = 0.04,
+						["hardwareDetent"] = false,
+						["hardwareDetentAB"] = 0,
+						["hardwareDetentMax"] = 0,
+						["invert"] = false,
+						["saturationX"] = 1,
+						["saturationY"] = 1,
+						["slider"] = false,
+					},
+					["key"] = "JOY_X",
+				},
+			},
+			["name"] = "LHC - TDC Slew Horizontal",
+		},
+		["a3226cd4"] = {
+			["added"] = {
+				[1] = {
+					["filter"] = {
+						["curvature"] = {
+							[1] = 0,
+						},
+						["deadzone"] = 0.04,
+						["hardwareDetent"] = false,
+						["hardwareDetentAB"] = 0,
+						["hardwareDetentMax"] = 0,
+						["invert"] = false,
+						["saturationX"] = 1,
+						["saturationY"] = 1,
+						["slider"] = false,
+					},
+					["key"] = "JOY_Y",
+				},
+			},
+			["name"] = "LHC - TDC Slew Vertical",
+		},
+		["a3325cd4"] = {
+			["added"] = {
+				[1] = {
+					["filter"] = {
+						["curvature"] = {
+							[1] = 0,
+						},
+						["deadzone"] = 0.04,
+						["hardwareDetent"] = false,
+						["hardwareDetentAB"] = 0,
+						["hardwareDetentMax"] = 0,
+						["invert"] = false,
+						["saturationX"] = 1,
+						["saturationY"] = 1,
+						["slider"] = false,
+					},
+					["key"] = "JOY_RX",
+				},
+			},
+			["name"] = "RHC - TDC Slew Horizontal",
+		},
+		["a3326cd4"] = {
+			["added"] = {
+				[1] = {
+					["filter"] = {
+						["curvature"] = {
+							[1] = 0,
+						},
+						["deadzone"] = 0.04,
+						["hardwareDetent"] = false,
+						["hardwareDetentAB"] = 0,
+						["hardwareDetentMax"] = 0,
+						["invert"] = false,
+						["saturationX"] = 1,
+						["saturationY"] = 1,
+						["slider"] = false,
+					},
+					["key"] = "JOY_RY",
+				},
+			},
+			["name"] = "RHC - TDC Slew Vertical",
+		},
+	},
+	["keyDiffs"] = {
+		["d3201pnilu3201cd4vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN12",
+					["reformers"] = {
+						[1] = "JOY_BTN5",
+					},
+				},
+			},
+			["name"] = "LHC - CMD Switch UP (Manual 2 Dispense)",
+		},
+		["d3202pnilu3202cd4vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN12",
+					["reformers"] = {
+						[1] = "JOY_BTN6",
+					},
+				},
+			},
+			["name"] = "LHC - CMD Switch DOWN (Manual 1 Dispense)",
+		},
+		["d3204pnilu3204cd4vd-1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_U",
+					["reformers"] = {
+						[1] = "JOY_BTN6",
+					},
+				},
+			},
+			["name"] = "LHC - Coolie Switch UP",
+		},
+		["d3205pnilu3205cd4vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_D",
+					["reformers"] = {
+						[1] = "JOY_BTN6",
+					},
+				},
+			},
+			["name"] = "LHC - Coolie Switch DOWN",
+		},
+		["d3206pnilu3206cd4vd-1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_L",
+					["reformers"] = {
+						[1] = "JOY_BTN6",
+					},
+				},
+			},
+			["name"] = "LHC - Coolie Switch LEFT",
+		},
+		["d3207pnilu3207cd4vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_R",
+					["reformers"] = {
+						[1] = "JOY_BTN6",
+					},
+				},
+			},
+			["name"] = "LHC - Coolie Switch RIGHT",
+		},
+		["d3209pnilu3209cd4vd-1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_U",
+					["reformers"] = {
+						[1] = "JOY_BTN5",
+					},
+				},
+			},
+			["name"] = "LHC - Castle Switch FWD",
+		},
+		["d3210pnilu3210cd4vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_D",
+					["reformers"] = {
+						[1] = "JOY_BTN5",
+					},
+				},
+			},
+			["name"] = "LHC - Castle Switch AFT",
+		},
+		["d3211pnilu3211cd4vd-1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_L",
+					["reformers"] = {
+						[1] = "JOY_BTN5",
+					},
+				},
+			},
+			["name"] = "LHC - Castle Switch LEFT",
+		},
+		["d3212pnilu3212cd4vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_R",
+					["reformers"] = {
+						[1] = "JOY_BTN5",
+					},
+				},
+			},
+			["name"] = "LHC - Castle Switch RIGHT",
+		},
+		["d3213pnilu3213cd4vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN7",
+					["reformers"] = {
+						[1] = "JOY_BTN5",
+					},
+				},
+			},
+			["name"] = "LHC - Castle Switch PRESS",
+		},
+		["d3219pnilu3219cd4vd-1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_U",
+				},
+			},
+			["name"] = "LHC - Auto Acquisition Switch FWD",
+		},
+		["d3220pnilu3220cd4vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_D",
+				},
+			},
+			["name"] = "LHC - Auto Acquisition Switch AFT",
+		},
+		["d3221pnilu3221cd4vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN7",
+				},
+			},
+			["name"] = "LHC - Auto Acquisition Switch PRESS",
+		},
+		["d3222pnilu3222cd4vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN9",
+					["reformers"] = {
+						[1] = "JOY_BTN5",
+					},
+				},
+			},
+			["name"] = "LHC - Trigger HA (Half)",
+		},
+		["d3223pnilu3223cd4vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN9",
+				},
+			},
+			["name"] = "LHC - Trigger FA (FUll",
+		},
+		["d3304pnilu3304cd4vd-1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN4",
+					["reformers"] = {
+						[1] = "JOY_BTN6",
+					},
+				},
+			},
+			["name"] = "RHC - Coolie Switch UP",
+		},
+		["d3305pnilu3305cd4vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN1",
+					["reformers"] = {
+						[1] = "JOY_BTN6",
+					},
+				},
+			},
+			["name"] = "RHC - Coolie Switch DOWN",
+		},
+		["d3306pnilu3306cd4vd-1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN3",
+					["reformers"] = {
+						[1] = "JOY_BTN6",
+					},
+				},
+			},
+			["name"] = "RHC - Coolie Switch LEFT",
+		},
+		["d3307pnilu3307cd4vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN2",
+					["reformers"] = {
+						[1] = "JOY_BTN6",
+					},
+				},
+			},
+			["name"] = "RHC - Coolie Switch RIGHT",
+		},
+		["d3309pnilu3309cd4vd-1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN4",
+					["reformers"] = {
+						[1] = "JOY_BTN5",
+					},
+				},
+			},
+			["name"] = "RHC - Castle Switch FWD",
+		},
+		["d3310pnilu3310cd4vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN1",
+					["reformers"] = {
+						[1] = "JOY_BTN5",
+					},
+				},
+			},
+			["name"] = "RHC - Castle Switch AFT",
+		},
+		["d3311pnilu3311cd4vd-1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN3",
+					["reformers"] = {
+						[1] = "JOY_BTN5",
+					},
+				},
+			},
+			["name"] = "RHC - Castle Switch LEFT",
+		},
+		["d3312pnilu3312cd4vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN2",
+					["reformers"] = {
+						[1] = "JOY_BTN5",
+					},
+				},
+			},
+			["name"] = "RHC - Castle Switch RIGHT",
+		},
+		["d3313pnilu3313cd4vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN8",
+					["reformers"] = {
+						[1] = "JOY_BTN5",
+					},
+				},
+			},
+			["name"] = "RHC - Castle Switch PRESS",
+		},
+		["d3319pnilu3319cd4vd-1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN4",
+				},
+			},
+			["name"] = "RHC - Auto Acquisition Switch FWD",
+		},
+		["d3320pnilu3320cd4vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN1",
+				},
+			},
+			["name"] = "RHC - Auto Acquisition Switch AFT",
+		},
+		["d3321pnilu3321cd4vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN8",
+				},
+			},
+			["name"] = "RHC - Auto Acquisition Switch PRESS",
+		},
+		["d3322pnilu3322cd4vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN10",
+					["reformers"] = {
+						[1] = "JOY_BTN6",
+					},
+				},
+			},
+			["name"] = "RHC - Trigger HA (Half)",
+		},
+		["d3323pnilu3323cd4vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN10",
+				},
+			},
+			["name"] = "RHC - Trigger FA (FUll",
+		},
+		["d350pnilu351cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN12",
+				},
+			},
+			["name"] = "Weapon Release (Pickle)",
+		},
+		["dnilp210u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Up Right slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_UR",
+				},
+			},
+		},
+		["dnilp211u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Down Right slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_DR",
+				},
+			},
+		},
+		["dnilp212u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Down Left slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_DL",
+				},
+			},
+		},
+		["dnilp213u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Up Left slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_UL",
+				},
+			},
+		},
+		["dnilp32u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Left slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_L",
+				},
+			},
+		},
+		["dnilp33u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Right slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_R",
+				},
+			},
+		},
+		["dnilp34u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Up slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_U",
+				},
+			},
+		},
+		["dnilp35u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Down slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_D",
+				},
+			},
+		},
+	},
+}
+return diff
